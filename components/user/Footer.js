0be4d657b0fb28258b996f0557c1cb695814634d
apisport <103476622+apisport@users.noreply.github.com>
2022-08-30 15:10:40 +0700

    Update Footer.js

--- components/user/Footer.js
+++ components/user/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-                <div className="border-color info-footer">
+                <div className="border-color info-footer text-center">
                     <div className="mx-auto d-flex flex-column flex-lg-row align-items-center footer-info-space gap-4">
                         <nav className="d-flex flex-lg-row flex-column align-items-center justify-content-center">
                             <p style={{ margin: 0 }}>Copyright © 2021 API SPORTS Team</p>
