cf449cada7f057058414e4c940dfb6687a884826
apisport <103476622+apisport@users.noreply.github.com>
2022-08-30 15:10:05 +0700

    Update Footer.js

--- components/user/Footer.js
+++ components/user/Footer.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div className="footer-2-1 container-xxl mx-auto position-relative p-0" style={{ fontFamily: '"Poppins", sans-serif' }}>
                 <div className="border-color info-footer">
                     <div className="mx-auto d-flex flex-column flex-lg-row align-items-center footer-info-space gap-4">
                         <nav className="d-flex flex-lg-row flex-column align-items-center justify-content-center">
@@ -11,7 +10,6 @@ const Footer = () => {
                         </nav>
                     </div>
                 </div>
-            </div>
         </>
     )
 }
